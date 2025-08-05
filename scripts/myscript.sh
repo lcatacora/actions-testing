77b0dd2a1cef56ac21bb977452def49b637af760
Luis Catacora <lcatacora@gmail.com>
2025-08-05 13:55:32 -0400

    updated names of env vars

--- scripts/myscript.sh
+++ scripts/myscript.sh
@@ -2,8 +2,7 @@
 echo "Hello from the shell script!"
 echo "This script is located at: $(realpath $0)"
 echo "Current working directory: $(pwd)"
-echo "Environment: $2"
-echo "Environment11: $3"
+echo
 echo "environment variables:"
 env
 echo
