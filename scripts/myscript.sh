29589202ac7b53828d291b3b775cc731c26fef8d
Luis Catacora <lcatacora@gmail.com>
2025-08-05 13:36:25 -0400

    added more debug statements and added the variables to the call to the script

--- scripts/myscript.sh
+++ scripts/myscript.sh
@@ -2,9 +2,12 @@
 echo "Hello from the shell script!"
 echo "This script is located at: $(realpath $0)"
 echo "Current working directory: $(pwd)"
-echo "Script execution completed."
+echo "Environment: $2"
+echo "Environment11: $3"
 echo "environment variables:"
 env
+echo
+echo "Script execution completed."
 exit 0
 # End of script
 
